3beaa81bc1145ae47cba900efd07e63b889cd707
sinagarafhael <sinagarafhael@gmail.com>
2025-09-11 14:40:45 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\InvoiceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('show');
 });
+
+
+// Company Registration
+Route::get('/register-company', [CompanyController::class, 'create']);
+Route::post('/register-company', [CompanyController::class, 'store']);
+
+// Products
+Route::get('/products', [ProductController::class, 'index']);
+
+// Orders
+Route::get('/orders/create', [OrderController::class, 'create']); // order form
+Route::post('/orders', [OrderController::class, 'store']);        // save order
+Route::get('/orders/{id}', [OrderController::class, 'show']);     // view order details
+Route::get('/my-orders', [OrderController::class, 'myOrders']);   // list company’s orders
+
+// Invoices
+Route::get('/invoice/{orderNumber}', [InvoiceController::class, 'show']);
+Route::get('/invoice/{orderNumber}/download', [InvoiceController::class, 'downloadPDF']);
