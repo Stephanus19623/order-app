6fe5eba48ac554178bee0b612a517e0feae9c1e9
sinagarafhael <sinagarafhael@gmail.com>
2025-09-15 08:25:57 +0700

    fourth commit

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Invoice extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'order_id',
+        'invoice_number',
+    ];
+
+    public function orderId(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'company_id',
+        'product_id',
+        'quantity',
+        'due_date',
+        'status',
+
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function quantity():HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
 }
